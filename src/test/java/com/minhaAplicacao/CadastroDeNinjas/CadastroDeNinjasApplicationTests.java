4b843343933597c51c2bf35e762c9afd2d403dcb
blackcode2301 <r2163306@gmail.com>
2026-06-03 15:07:17 -0300

    primeiro commit

---
+++ src/test/java/com/minhaAplicacao/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaAplicacao.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
