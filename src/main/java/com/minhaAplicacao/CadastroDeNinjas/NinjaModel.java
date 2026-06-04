a4e0805ed4b8072a05f7f4040a8de6d25aa4a4eb
blackcode2301 <r2163306@gmail.com>
2026-06-04 15:30:21 -0300

    readme

--- src/main/java/com/minhaAplicacao/CadastroDeNinjas/NinjaModel.java
+++ src/main/java/com/minhaAplicacao/CadastroDeNinjas/NinjaModel.java
@@ -14,10 +14,10 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id;
-    String nome;
-    int idade;
-    String email;
+    private long id;
+    private String nome;
+    private int idade;
+    private String email;
 
     NinjaModel() {
 
