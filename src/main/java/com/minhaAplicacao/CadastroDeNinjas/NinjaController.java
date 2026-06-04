e1949de104a0a26e6a2756997da96be9425afc0a
blackcode2301 <r2163306@gmail.com>
2026-06-04 15:28:04 -0300

    jpa e database

--- src/main/java/com/minhaAplicacao/CadastroDeNinjas/NinjaController.java
+++ src/main/java/com/minhaAplicacao/CadastroDeNinjas/NinjaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
 
     /*
      * public String getMethodName(@RequestParam String param) {
---
+++ src/main/java/com/minhaAplicacao/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,57 @@
+package com.minhaAplicacao.CadastroDeNinjas;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// entity transforma uma classe em uma entidade do database
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String nome;
+    int idade;
+    String email;
+
+    NinjaModel() {
+
+    }
+
+    NinjaModel(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
