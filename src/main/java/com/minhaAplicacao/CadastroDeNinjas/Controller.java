06682e032a64ca5bbe88abebd90f101a01c33627
blackcode2301 <r2163306@gmail.com>
2026-06-04 14:13:09 -0300

    resovendo dependencias

--- src/main/java/com/minhaAplicacao/CadastroDeNinjas/Controller.java
+++ src/main/java/com/minhaAplicacao/CadastroDeNinjas/Controller.java
@@ -3,15 +3,21 @@ package com.minhaAplicacao.CadastroDeNinjas;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping
 public class Controller {
 
+    /*
+     * public String getMethodName(@RequestParam String param) {
+     * return new String();
+     * }
+     */
+
     @GetMapping("/boasvindas")
     public String boasvindas() {
-        return "testeando";
+        return "primeira mensagem dessa rota";
     }
 
 }
